350e410f21d42f158a48a02880ca2362da03cd21
Paul Scott <pscott@inductiveautomation.com>
2023-03-21 18:37:52 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: "Ignition SDK Programmer's Guide",
   tagline: 'Develop Ignition Modules using the Ignition Module SDK',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://main.d2jv2u93hgjlfb.amplifyapp.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
