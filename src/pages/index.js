573ff1543bab278216756616f08941734bbb70f8
root <root@SU-5MQCMH3-WS.ia.local>
2023-08-28 12:09:14 -0700

    Researched Docusaurus pages vs docs and it appears as if the layout for the current index page does not get affected by the themes applied to docs pages where we can easily insert the searchbar. to insert the searchbar into the homepage index at / , i built a react component that loads in the required cdn content and then calls the appropriate method supplied from the content to inject the docsearch component. Unfortunately we have not found a way to inject directly into the nav bar via some established API, instead we may just have to modify the DOM with JS or otherwise modify the docusaurus library source

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,9 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import { Helmet } from 'react-helmet';
-import { docsearchConfig } from '../pages/docsearch_config';
+import SearchBarComponentInjector from '../components/SearchBarComponentInjector/SearchBarComponentInjector';
 
-import styles from './index.module.css';
+import styles from '.';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -16,7 +16,7 @@ function HomepageHeader() {
       <div className="container">
         <Helmet>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/typesense-docsearch-css@0.3.0" />
-        </Helmet>
+        </Helmet>       
         <h0 className="hero__title">{siteConfig.title}</h0>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
@@ -32,31 +32,20 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const [docSearchInjected, setDocSearchInjected] = useState(false);
-
-  useEffect(() => {
-    if (!docSearchInjected) {
-      const script = document.createElement('script');
-      script.src = 'https://cdn.jsdelivr.net/npm/typesense-docsearch.js@3.4';
-      script.async = true;
-      script.onload = () => {
-        window.docsearch(docsearchConfig);
-        setDocSearchInjected(true);
-      };
-      document.head.appendChild(script);
-    }
-  }, [docSearchInjected]);
-  
+   
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-                <div id='searchbar' className={styles.searchbar}></div>
-
+              
       <HomepageHeader />
+
       <main>
+
+        
         <HomepageFeatures />
+     
       </main>
     </Layout>
   );
