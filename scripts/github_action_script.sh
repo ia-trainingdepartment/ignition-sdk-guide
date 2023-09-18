47c2fc49b9d2f3f89c516b24a965eaaba0f58c36
root <root@SU-5MQCMH3-WS.ia.local>
2023-09-18 15:17:15 -0700

    Testing with script in its own file in /scripts

--- scripts/github_action_script.sh
+++ scripts/github_action_script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 echo "Deploy app $aws_app_id branch $branch_name"
-jq --version
-aws amplify start-job --app-id $aws_app_id --branch-name $branch_name --job-type RELEASE
-JOB_ID=$(aws amplify start-job --app-id $aws_app_id --branch-name $branch_name --job-type RELEASE | jq -r '.jobSummary.jobId')
+JOB_ID=$(aws amplify start-job --app-id $aws_app_id --branch-name $branch_name --job-type RELEASE --query 'jobSummary.jobId' --output text)
 echo "Release started"
 echo "Job ID is $JOB_ID"
-while [[ "$(aws amplify get-job --app-id $aws_app_id --branch-name $branch_name --job-id $JOB_ID | jq -r '.job.summary.status')" =~ ^(PENDING|RUNNING)$ ]]; do sleep 1; done
-JOB_STATUS="$(aws amplify get-job --app-id $aws_app_id --branch-name $branch_name --job-id $JOB_ID | jq -r '.job.summary.status')"
+while [[ "$(aws amplify get-job --app-id $aws_app_id --branch-name $branch_name --job-id $JOB_ID --query 'job.summary.status' --output text)" =~ ^(PENDING|RUNNING)$ ]]; do sleep 1; done
+JOB_STATUS="$(aws amplify get-job --app-id $aws_app_id --branch-name $branch_name --job-id $JOB_ID --query 'job.summary.status' --output text)"
 echo "Job finished"
 echo "Job status is $JOB_STATUS"
