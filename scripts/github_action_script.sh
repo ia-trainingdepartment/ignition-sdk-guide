829ffec2c0e3d43fe2bee4f3f6ea5d9596506782
root <root@SU-5MQCMH3-WS.ia.local>
2023-09-18 15:45:56 -0700

    Testing with script in its own file in /scripts

--- scripts/github_action_script.sh
+++ scripts/github_action_script.sh
@@ -2,18 +2,10 @@
 
 echo "Deploy app $aws_app_id branch $branch_name"
 
-# Debugging statement
-echo "Output from start-job command:"
-aws amplify start-job --app-id $aws_app_id --branch-name $branch_name --job-type RELEASE --query 'jobSummary.jobId' --output text
-
 JOB_ID=$(aws amplify start-job --app-id $aws_app_id --branch-name $branch_name --job-type RELEASE --query 'jobSummary.jobId' --output text)
 
 echo "Release started"
 echo "Job ID is $JOB_ID"
-
-# Debugging statement
-echo "Output from get-job command:"
-aws amplify get-job --app-id $aws_app_id --branch-name $branch_name --job-id $JOB_ID --query 'job.summary.status' --output text
 
 while [[ "$(aws amplify get-job --app-id $aws_app_id --branch-name $branch_name --job-id $JOB_ID --query 'job.summary.status' --output text)" =~ ^(PENDING|RUNNING)$ ]]; do sleep 1; done
 
