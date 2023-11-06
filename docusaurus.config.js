a586191a3cd86c1425e6a7d026ca37660ac6d9fd
ia-sshamgar <108418969+ia-sshamgar@users.noreply.github.com>
2023-11-06 08:30:41 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -216,60 +216,60 @@ const config = {
 //           },
 //         ],
 //       },
-//       footer: {
-//         style: 'dark',
-//         links: [
-//           {
-//             title: 'Docs',
-//             items: [
-//               {
-//                 label: 'SDK Guide',
-//                 to: '/docs/intro',
-//               },
-//               {
-//                 label: 'User Manual',
-//                 href: 'https://docs.inductiveautomation.com/display/DOC81',
-//               },
-//             ],
-//           },
-//           {
-//             title: 'Community',
-//             items: [
-//               {
-//                 label: 'Stack Overflow',
-//                 href: 'https://stackoverflow.com/questions/tagged/ignition',
-//               },
-//               {
-//                 label: 'Forums',
-//                 href: 'https://forum.inductiveautomation.com/',
-//               },
-//               {
-//                 label: 'Twitter',
-//                 href: 'https://twitter.com/InductiveAuto',
-//               },
-//             ],
-//           },
-//           {
-//             title: 'More',
-//             items: [
-//               {
-//                 label: 'Blog',
-//                 to: '/blog',
-//               },
-//               {
-//                 label: 'GitHub',
-//                 href: 'https://github.com/ia-trainingdepartment/ignition-sdk-guide',
-//               },
-//             ],
-//           },
-//         ],
-//         copyright: `Copyright © ${new Date().getFullYear()} Inductive Automation, Inc. Built with Docusaurus.`,
-//       },
-//       prism: {
-//         theme: lightCodeTheme,
-//         darkTheme: darkCodeTheme,
-//       },
-//     }),
+      footer: {
+         style: 'dark',
+         links: [
+           {
+             title: 'Docs',
+             items: [
+               {
+                 label: 'SDK Guide',
+                 to: '/docs/intro',
+               },
+               {
+                 label: 'User Manual',
+                 href: 'https://docs.inductiveautomation.com/display/DOC81',
+               },
+             ],
+           },
+           {
+             title: 'Community',
+             items: [
+               {
+                 label: 'Stack Overflow',
+                 href: 'https://stackoverflow.com/questions/tagged/ignition',
+               },
+               {
+                 label: 'Forums',
+                 href: 'https://forum.inductiveautomation.com/',
+               },
+               {
+                 label: 'Twitter',
+                 href: 'https://twitter.com/InductiveAuto',
+               },
+             ],
+           },
+           {
+             title: 'More',
+             items: [
+               {
+                 label: 'Blog',
+                 to: '/blog',
+               },
+               {
+                 label: 'GitHub',
+                 href: 'https://github.com/ia-trainingdepartment/ignition-sdk-guide',
+               },
+             ],
+           },
+         ],
+         copyright: `Copyright © ${new Date().getFullYear()} Inductive Automation, Inc. Built with Docusaurus.`,
+       },
+       prism: {
+         theme: lightCodeTheme,
+         darkTheme: darkCodeTheme,
+       },
+     }),
 };
 
 module.exports = config;
